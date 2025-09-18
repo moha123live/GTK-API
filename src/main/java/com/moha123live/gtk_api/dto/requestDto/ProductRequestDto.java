7e877e9d8da138e370cbdc48d837e9b39ebbb3b8
Mohan Kumar <moha123live@gmail.com>
2025-09-18 11:31:30 +0530

    Product response dto updated

--- src/main/java/com/moha123live/gtk_api/dto/requestDto/ProductRequestDto.java
+++ src/main/java/com/moha123live/gtk_api/dto/requestDto/ProductRequestDto.java
@@ -2,6 +2,7 @@ package com.moha123live.gtk_api.dto.requestDto;
 
 import java.math.BigDecimal;
 import com.moha123live.gtk_api.model.Product;
+import com.moha123live.gtk_api.util.AppMessages;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,17 +12,17 @@ import lombok.Data;
 @Data
 public class ProductRequestDto {
 
-    @NotBlank(message = "Product name is required")
-    @Size(max = 100, message = "Name must not exceed 100 characters")
+    @NotBlank(message = AppMessages.PRODUCT_NAME_REQUIRED)
+    @Size(max = 100, message = AppMessages.PRODUCT_NAME_LENGTH)
     private String name;
 
-    @Size(max = 100, message = "Tamil name must not exceed 100 characters")
+    @Size(max = 100, message = AppMessages.PRODUCT_TAMIL_NAME_LENGTH)
     private String tamilName;
 
-    @NotNull(message = "Unit is required")
+    @NotNull(message = AppMessages.PRODUCT_UNIT_REQUIRED)
     private Product.Unit unit;
 
-    @DecimalMin(value = "0.00", inclusive = true, message = "Commission must be >= 0")
+    @DecimalMin(value = "0.00", inclusive = true, message = AppMessages.PRODUCT_COMMISSION_GREATER_THAN_ZERO)
     private BigDecimal comm2;
 
     private Product.Status status;
